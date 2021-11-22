04ff8690910b4a521eda08aa4ae3c85491c10604
Utkarsh Singh <ut254_7@hotmail.com>
2021-11-21 18:51:06 -0600

    Added the css gradient for sign up page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {AppContainer} from './styles';
+import { AccountBox } from './Components/AccountBox';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <AppContainer>
+    <AccountBox/>
+    </AppContainer>
 }
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+
+export const AppContainer=styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap');   
+    @import url("https://fonts.googleapis.com/css2?family=Pangolin&display=swap");
+    & h1 {
+        font-family: "Montserrat", sans-serif;
+    }
+    & h2 {
+        font-family: "Pangolin", sans-serif;
+    }
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:center;
+`;
