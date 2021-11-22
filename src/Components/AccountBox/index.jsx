d4f24262fd79941f15b119f3e6ba990a1862f343
Utkarsh Singh <ut254_7@hotmail.com>
2021-11-21 19:42:16 -0600

    Created Reusable Components for Sign up and Log in

--- src/Components/AccountBox/index.jsx
+++ src/Components/AccountBox/index.jsx
@@ -1,39 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
-const BoxContainer=styled.div`
-    width: 280px;
-    min-height: 550px;
-    display:flex;
-    flex-direction:column;
-    border-radius: 19px;
-    background-color: rgba(255,255,255,100);
-    box-shadow: 0 0 2px rgba(15,15,15,0.30);
-    position: relative;
-    overflow: hidden;
-`;
-const TopContainer=styled.div`
-    width:100%;
-    height:250px;
-    display:flex;
-    flex-direction:column;
-    justify-content: flex-end;
-    padding:0 1.8em;
-    padding-bottom: 5em;
-`;
-const BackDrop=styled.div`
-    width:160%;
-    height:600px;
-    position:absolute;
-    display:flex;
-    flex-direction:column;
-    border-radius:50%;
-    background: rgb(181,77,241);
-    background: linear-gradient(90deg, rgba(181,77,241,1) 27%, rgba(0,252,253,0.86) 100%);  
-`;
+import { BoxContainer,TopContainer,BackDrop,HeaderContainer,HeaderText} from '../../styles';
 export function AccountBox(props){
     return <BoxContainer>
         <TopContainer>
             <BackDrop />
+            <HeaderContainer>
+                <HeaderText> Welcome</HeaderText>
+                <HeaderText> To</HeaderText>
+                <HeaderText> Carzz 3330</HeaderText>
+            </HeaderContainer>
         </TopContainer>
     </BoxContainer>
 }
--- src/styles.js
+++ src/styles.js
@@ -17,3 +17,49 @@ export const AppContainer=styled.div`
     align-items:center;
     justify-content:center;
 `;
+export const BoxContainer=styled.div`
+    width: 280px;
+    min-height: 550px;
+    display:flex;
+    flex-direction:column;
+    border-radius: 19px;
+    background-color: rgba(255,255,255,100);
+    box-shadow: 0 0 2px rgba(15,15,15,0.30);
+    position: relative;
+    overflow: hidden;
+`;
+export const TopContainer=styled.div`
+    width:100%;
+    height:250px;
+    display:flex;
+    flex-direction:column;
+    justify-content: flex-end;
+    padding:0 1.8em;
+    padding-bottom: 5em;
+`;
+export const BackDrop=styled.div`
+    width:160%;
+    height:600px;
+    position:absolute;
+    display:flex;
+    flex-direction:column;
+    border-radius:50%;
+    top: -300px;
+    left: -70px;
+    background: rgb(181,77,241);
+    background: linear-gradient(90deg, rgba(181,77,241,1) 27%, rgba(0,252,253,0.86) 100%);  
+`;
+export const HeaderContainer=styled.div`
+    width:100%;
+    display:flex;
+    flex-direction:column;
+`;
+export const HeaderText=styled.h2`
+    font-size:35px;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 600;
+    line-height:1.24;
+    color: rgba(236, 240, 241,1.0);
+    z-index:10;
+    margin:0;
+`;
