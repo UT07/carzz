3e69c53cad0aedb40e206b88b9152d213e4e614d
Utkarsh Singh <ut254_7@hotmail.com>
2021-12-03 21:54:56 -0600

    fixed the default route

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -34,7 +34,7 @@ export function Navbar(props){
         </Menu.Item>
         <Menu.Item onClick={()=>{
             localStorage.removeItem('customer');
-            window.location.href='/login'
+            window.location.href='/'
         }}>
             <li style={{color:"#CA0046"}}>Logout</li>
         </Menu.Item>
